420a3a70b3b3204bbb00c0ded3542ad7b03dd5f4
JinhaKangLAB <75330517+JinhaKangLAB@users.noreply.github.com>
2021-01-21 21:26:20 +0900

    Create Operator.c

---
+++ Operator.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+//Addition example
+/*int main(void){
+	int num1 = 3;
+	int num2 = 4;
+	int result = num1 + num2;
+
+	printf("Result : %d \n" , result);
+	printf("%d + %d = %d \n" , num1, num2, result);
+	printf("%d plus %d is %d. \n", num1, num2, result);
+	return 0;
+} */
+
+//Binary operator
+/*int main(void){
+	int num1=9, num2=2;
+	printf("%d + %d = %d \n", num1, num2, num1+num2);
+	printf("%d - %d = %d \n", num1, num2, num1-num2);
+	printf("%d * %d = %d \n", num1, num2, num1*num2);
+	printf("%d / %d = %d \n", num1, num2, num1/num2);
+	printf("%d %% %d = %d \n", num1, num2, num1%num2);
+	return 0;
+} */
+
+//Operator test
+/*int main(void){
+	int num1=2, num2=4, num3=6;
+	num1 += 3; // num1 = num1 + 3;
+	num2 *= 4; // num2 = num2 * 4;
+	num3 %= 5; // num3 = num3 % 5;
+	printf("Result: %d, %d, %d, \n", num1, num2, num3);
+	return 0;
+} */
+
+//Exchange singed number
+/*int main(void){
+	int num1 = +2;
+	int num2 = -4;
+
+	num1 = -num1;
+	printf("num1 : %d \n", num1); 
+	num2 = -num2;
+	printf("num2 : %d \n", num2);
+	printf("Memory address: %p \n", &num1);
+	return 0;
+} */
+
+//
+/*int main(void){
+	int num1=12;
+	int num2=12;
+
+	printf("num1: %d \n", num1);
+	printf("num1++: %d \n", num1++);
+	printf("num1: %d \n\n", num1);
+
+	printf("num2: %d \n", num2);
+	printf("++num2: %d \n", ++num2);
+	printf("num2: %d \n", num2);
+	return 0;
+} */
+
+/*int main(void){
+	int num1 = 10;
+	int num2=(num1--)+2;
+
+	printf("num1: %d \n", num1);
+	printf("num2: %d \n", num2);
+	return 0;
+} */
+
+//
+/*int main(void){
+	int num1=10;
+	int num2=12;
+	int result1, result2, result3;
+
+	result1=(num1==num2);
+	result2=(num1<=num2);
+	result3=(num1>num2);
+
+	printf("result1: %d \n", result1);
+	printf("result2: %d \n", result2);
+	printf("result3: %d \n", result3);
+	return 0;
+} */
+
+//Logical Operator
+/*int main(void){
+	int num1=10;
+	int num2=12;
+	int result1, result2, result3;
+	result1 = (num1==10 && num2==12);
+	result2 = (num1<12 || num2>12);
+	result3 = (!num1);
+
+	printf("result1: %d \n", result1);
+	printf("resutl2: %d \n", result2);
+	printf("result3: %d \n", result3);
+	return 0;
+} */
+

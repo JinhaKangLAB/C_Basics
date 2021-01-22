905ec2e645280acfde7f7925de0bcba2351643fb
JinhaKangLAB <75330517+JinhaKangLAB@users.noreply.github.com>
2021-01-22 21:26:39 +0900

    Create data type practice.c

---
+++ data type practice.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+/*int main(void){
+	char ch=9;
+	int inum=1052;
+	double dnum=3/1415;
+	printf("size of ch : %d \n", sizeof(ch));
+	printf("size of inum : %d \n", sizeof(inum));
+	printf("size of dnum : %d \n", sizeof(dnum));
+
+	printf("size of char : %d \n", sizeof(char));
+	printf("size of int : %d \n", sizeof(int));
+	printf("size of long : %d \n", sizeof(long));
+	printf("size of long long : %d \n", sizeof(long long));
+	printf("size of float : %d \n", sizeof(float));
+	printf("size of double : %d \n", sizeof(double));
+	return 0;
+} */
+
+//CharShortBaseAdd
+/* int main(void){
+	char num1 = 1, num2 = 2, result1 = 0;
+	short num3 = 300,  num4 = 400, result2 = 0;
+
+	printf("size of num1 & num2 : %d, %d \n", sizeof(num1), sizeof(num2));
+	printf("size of num3 & num4 : %d, %d \n", sizeof(num3), sizeof(num4));
+
+	printf("size of char add : %d \n", sizeof(num1+num2));
+	printf("size of short add : %d \n", sizeof(num3+num4));
+
+	result1=num1+num2;
+	result2=num3+num4;
+	printf("size of result1 & result2 : %d, %d \n", sizeof(result1), sizeof(result2));
+	return 0;
+} */
+
+//
+/*int main(void){
+	 double rad;
+	 double area;
+	 printf("Enter the radius of the circle : ");
+	 scanf("%lf", &rad);
+
+	 area = rad*rad*3.1415;
+	 printf("The area of the circle : %f \n", area);
+	 return 0;
+} */
+
+/*int main(void){
+	char ch1='A', ch2=65;
+	int ch3='Z', ch4=90;
+
+    printf("%c %d \n", ch1, ch1);
+	printf("%c %d \n", ch2, ch2);
+	printf("%c %d \n", ch3, ch3);
+	printf("%c %d \n", ch4, ch4);
+	return 0;
+}*/
+
+//Printing float number
+/*int main(void){
+	float num;
+	printf("Enter the float number \n");
+	scanf("%f",&num);
+    printf("You entered : %f", num);
+	return 0;
+} */
+
+//
+/*int main(void){
+	double num1=245;
+	int num2=3.1415;
+	int num3=129;
+	char ch=num3;
+
+	printf("int 245 to float: %f \n", num1);
+	printf("float 3.1415 to int: %d \n", num2);
+	printf("large int 129 to small int: %d \n", ch);
+	return 0;
+} */
+
+//ConvDiv
+/*int main(void){
+	int num1=3, num2=4;
+	double divResult;
+	//divResult = num1/num2; //Result:0.000000
+	divResult = (double)num1/num2;
+	printf("Result of div : %f \n", divResult);
+	return 0;
+} */
+
+

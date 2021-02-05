4cd7c9bb0869faa7fbfc4d69f2e0516c93544880
JinhaKangLAB <75330517+JinhaKangLAB@users.noreply.github.com>
2021-02-05 20:21:59 +0900

    Update total and average using arrary.c

--- total and average using arrary.c
+++ total and average using arrary.c
@@ -15,3 +15,20 @@ int main(void){
 
 	return 0;
 }
+
+//random function
+/*#include <stdlib.h>   
+int main(void){
+	int score[1000], i, sum=0;
+
+	for(i=0;i<1000;i++){
+	score[i]= rand() % 100+1;
+	printf("%d\n", score[i]);
+	sum+= score[i];
+	}
+
+	printf("Total : %d \nAverage : %.2f \n", sum, sum/1000.0); //자동 형 변환
+
+	return 0;
+
+} */

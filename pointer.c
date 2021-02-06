9bb1f4067cacbe58d6fe5dc4e20cfcce4ff7232e
JinhaKangLAB <75330517+JinhaKangLAB@users.noreply.github.com>
2021-02-06 21:03:01 +0900

    Update pointer.c

--- pointer.c
+++ pointer.c
@@ -34,14 +34,46 @@
 	return 0;
 } */
 
-int main(void){
+/*int main(void){
 	float f=10.3;
 	float *p;
 
 	printf(" Before : %.1f\n", f);
 	p = &f;		
 	*p=30.5;
-	printf(" After : %.1f\n", f);						
+	printf(" After : %.1f\n", f);	
+
+	*&f = 40.2f;
+	printf("%.1f\n", f);
 
 	return 0;
+} */
+
+/* int main()
+{
+	int a;
+	int *pa;
+	pa= &a;
+	*pa=10;
+
+	printf("output 'a' with pointer :  %d\n" , *pa);
+	printf("oupt put 'a' with variation name : %d\n", a);
+} */
+
+int main(){
+	int a = 10, b =15, tot;
+	double avg;
+	int *pa, *pb;
+	int *pt = &tot;
+	double *pg=&avg;
+
+	pa = &a;
+	pb = &b;
+
+	*pt = *pa + *pb;
+	*pg = *pt / 2.0;
+
+	printf(" %d %d\n", *pa, *pb);
+	printf(" Total : %d\n", *pt);
+	printf(" Average : %.1f\n", *pg);
 }

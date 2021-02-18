1fb101b1627d9f0e2dc07d501048db48e8ec90ad
JinhaKangLAB <75330517+JinhaKangLAB@users.noreply.github.com>
2021-02-18 21:25:12 +0900

    Update pointer and function.c

--- pointer and function.c
+++ pointer and function.c
@@ -16,7 +16,7 @@ int main(void){
 	return 0;} */
 
 //Array Param Access
-void ShowArrayElem( int  * param, int len){
+/*void ShowArrayElem( int  * param, int len){
 	int i;
 	for(i=0;i<len;i++)
 		printf("%d ", param[i]);
@@ -36,4 +36,55 @@ int main(void){
 	AddArrayElem(arr, sizeof(arr)/sizeof(int),3);
 	ShowArrayElem(arr, sizeof(arr)/sizeof(int));
 	return 0;
+}*/
+
+//Call by Value Swap
+/*void Swap(int n1, int n2){
+	int temp=n1;
+	n1=n2;
+	n2=temp;
+	printf("n1 n2 : %d %d \n", n1, n2);
 }
+int main(void){
+	int num1=10;
+	int num2=20;
+	printf("num1 num2 : %d %d \n", num1, num2);
+
+	Swap(num1, num2);
+	printf("num1 num2 : %d %d \n", num1, num2);
+	return 0;
+} */
+
+//CallbyReference
+/*void Swap(int * ptr1, int * ptr2)
+{ 
+	int temp = * ptr1;
+	* ptr1 = * ptr2;
+	* ptr2 = temp;
+}
+
+int main(void){
+	int num1 = 10;
+	int num2 = 20;
+	printf("num1 num2 : %d %d \n", num1, num2);
+	Swap(&num1, &num2);
+	printf("num1 num2 : %d %d \n", num1, num2);
+	return 0;
+} */
+
+/*int main(void){
+	double PI=3.1415;
+	double rad;
+	PI=3.07;
+	scanf("%lf", &rad);
+	printf("circle are %f \n", rad*rad*PI);
+	return 0;
+}*/
+/* int main(void){
+	const double PI=3.1415;
+	double rad;
+	PI=3.07; //error
+	scanf("%lf", &rad);
+	printf("circle are %f \n", rad*rad*PI);
+	return 0;
+} */

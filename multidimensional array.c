312577f4402093322dfc53b70285aacba3a18893
JinhaKangLAB <75330517+JinhaKangLAB@users.noreply.github.com>
2021-02-19 20:09:33 +0900

    Create multidimensional array.c

---
+++ multidimensional array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/*int main(void){
+	int villa[4][2];
+	int popu, i, j;
+	for(i=0;i<4;i++){
+		for(j=0;j<2;j++){
+			printf("The number of people at %d floor %d unit is :  ", i+1, j+1);
+			scanf("%d", &villa[i][j]);
+		}
+	}
+
+	for(i=0; i<4; i++){
+		popu = 0;
+		popu += villa[i][0];
+		popu += villa[i][1];
+		printf("The number of people at %d floor : %d \n", i+1, popu);
+	}
+	return 0;
+}*/
+
+/*int main(void)
+{
+	int arr[3][2];
+	int i, j;
+	for(i=0; i<3; i++)
+		for(j=0; j<2; j++)
+			printf("%p  \n", &arr[i][j]);
+	return 0;
+} */
+
